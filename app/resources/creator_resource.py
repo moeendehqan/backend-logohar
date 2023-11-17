f9bcf4378b233e563c72da91facbf71c7fc246cd
moeendehqan <moeen.dehqan@gmail.com>
2023-11-17 22:45:52 +0330

    creator doing

--- app/resources/creator_resource.py
+++ app/resources/creator_resource.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource, reqparse, request
+from app.service.validation_servise import  validation_service
+from app.service.nlp_service import sent_vector
 
 
 creator_parser = reqparse.RequestParser()
@@ -14,5 +16,20 @@ creator_parser.add_argument('pallet',type=str, required=True, help='پالت ا�
 class creator_resource(Resource):
     def post(self):
         args = creator_parser.parse_args()
+        obj_valid = validation_service()
+        name_valid = obj_valid.BusinessName(args['lang'], args['dispalyName'], args['persianName'])
+        if name_valid[1] != 200:
+            return name_valid
+        list_validation = [obj_valid.contains_only_persian(i) for i in args['keywords']]
+        if False in list_validation:
+            return {'result':False,'message':'کلید واژه ها میبایست به فارسی باشد'},200
+        object_sent_vector = sent_vector()
+        
+        if args['lang'] == 'persian':
+            args['name_vector'] = object_sent_vector.word_normaloize_and_vector_list(args['dispalyName'])
+        else:
+            args['name_vector'] = object_sent_vector.word_normaloize_and_vector_list(args['persianName'])
         print(args)
+        
+
         return True
